107243c76edbd0706a8f39f2eb234079331d79c9
Bondarev Artem <bond210484@mail.ru>
2025-06-19 15:44:12 +0700

    fix error

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 // TODO: write code here
+import img from '../images/goblin.png';
 const gameField = document.querySelector('.game-field');
 
 for (let i = 0; i < 16; i++) {
@@ -8,11 +9,17 @@ for (let i = 0; i < 16; i++) {
 }
 
 const character = document.createElement('img');
-character.src = '../src/images/goblin.png';
+character.src = img;
 
 function placeCharacter() {
   const cells = document.querySelectorAll('.cell');
-  const randomIndex = Math.floor(Math.random() * cells.length);
-  cells[randomIndex].appendChild(character);
+  let randomIndex = Math.floor(Math.random() * cells.length);
+  const currentIndex = Array.from(cells).indexOf(character.parentNode);
+  do {if (randomIndex !== currentIndex) {
+    cells[randomIndex].appendChild(character);
+    break; 
+  }
+  randomIndex = Math.floor(Math.random() * cells.length); 
+} while (true);
 }
 setInterval(placeCharacter, 1000);
